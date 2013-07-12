994bc20c542eda8f02f0d4a8098dde3d55973a40
Ilesh Patel <ilesh21190@gmail.com>
2013-07-12 15:11:21 +0530

    Grails Constraints and Simple Racetrack Application

---
+++ Racetrack/grails-app/domain/racetrack/Race.groovy
@@ -0,0 +1,31 @@
+package racetrack
+
+class Race {
+
+	String name 
+	Date startDate 
+	String city 
+	String state 
+	BigDecimal distance 
+	BigDecimal cost 
+	Integer maxRunners = 100000
+    
+		static constraints = {
+		name(blank:false,maxSize:50) 
+		startDate(validator:{return (it>new Date())}) 
+		city() 
+		state(inList:["Guj","Mh","Delhi"]) 
+		distance(min:0.0,max:500.0) 
+		cost(min:0.0,max:100.0) 
+		maxRunners(min:0,max:10000) 
+		
+    }
+	BigDecimal inMiles()
+	{
+		return distance*0.6214
+	}
+    
+	static mapping= {
+		sort "startDate"
+	}
+}
---
+++ Racetrack/grails-app/domain/racetrack/Registration.groovy
@@ -0,0 +1,18 @@
+package racetrack
+
+class Registration {
+
+	String name 
+	Date dateOfBirth 
+	String gender 
+	String address 
+	String city 
+	String state 
+	String zipcode 
+	String email 
+	Date dateCreated
+    
+	static mapping = { 
+		autoTimestamp false 
+	} 
+}
